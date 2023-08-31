023bc2e2712b5d6db90ab0d7bd214155671daa5d
Urjit Singh Bhatia <urjitsb87@gmail.com>
2023-08-30 17:05:48 -0700

    simplify test setup

--- client_test.go
+++ client_test.go
@@ -8,11 +8,6 @@ import (
 
 var _ = Describe("Client", func() {
 	Describe("Connection", func() {
-		It("connects properly", func() {
-			_, err := chroma.NewClient("http://localhost:8000")
-			Expect(err).ToNot(HaveOccurred())
-		})
-
 		It("rejects bad server urls", func() {
 			_, err := chroma.NewClient("http\n://foo.com/")
 			Expect(err).To(HaveOccurred())
@@ -20,65 +15,48 @@ var _ = Describe("Client", func() {
 	})
 
 	It("gets heartbeat", func() {
-		client, err := chroma.NewClient("http://localhost:8000")
-		Expect(err).ToNot(HaveOccurred())
-
-		alive, err := client.Heartbeat()
+		alive, err := testClient.Heartbeat()
 		Expect(err).ToNot(HaveOccurred())
 		Expect(alive).To(BeNumerically(">", 0))
 
 	})
 
 	It("resets the db", func() {
-		client, err := chroma.NewClient("http://localhost:8000")
-		Expect(err).ToNot(HaveOccurred())
-
-		ok, err := client.Reset()
+		ok, err := testClient.Reset()
 		Expect(err).ToNot(HaveOccurred())
 		Expect(ok).To(BeTrue())
 	})
 
 	It("gets the version of the db", func() {
-		client, err := chroma.NewClient("http://localhost:8000")
-		Expect(err).ToNot(HaveOccurred())
-
-		ver, err := client.GetVersion()
+		ver, err := testClient.GetVersion()
 		Expect(err).ToNot(HaveOccurred())
 		Expect(ver).To(Equal("0.4.8"))
 	})
 
 	Describe("collections", func() {
 		BeforeEach(func() {
-			client, err := chroma.NewClient("http://localhost:8000")
-			Expect(err).ToNot(HaveOccurred())
-			client.Reset()
+			testClient.Reset()
 		})
 
 		It("list collections", func() {
-			client, err := chroma.NewClient("http://localhost:8000")
-			Expect(err).ToNot(HaveOccurred())
-
-			collections, err := client.ListCollections()
+			collections, err := testClient.ListCollections()
 			Expect(err).ToNot(HaveOccurred())
 			Expect(len(collections)).To(Equal(0))
 		})
 
 		It("create and then list collection", func() {
-			client, err := chroma.NewClient("http://localhost:8000")
-			Expect(err).ToNot(HaveOccurred())
-
 			// create new collection
-			collection, err := client.CreateCollection("unit-test", "l2", nil)
+			collection, err := testClient.CreateCollection("unit-test", "l2", nil)
 			Expect(err).ToNot(HaveOccurred())
 			Expect(collection.Name).To(Equal("unit-test"))
 
 			// should error if recreating existing collection
-			_, err = client.CreateCollection("unit-test", "l2", nil)
+			_, err = testClient.CreateCollection("unit-test", "l2", nil)
 			Expect(err).To(HaveOccurred())
 			Expect(err.Error()).To(Equal("error while creating collection: ValueError('Collection unit-test already exists.')"))
 
 			// list the collections
-			collections, err := client.ListCollections()
+			collections, err := testClient.ListCollections()
 			Expect(err).ToNot(HaveOccurred())
 			Expect(len(collections)).To(Equal(1))
 			Expect(collections[0].Name).To(Equal("unit-test"))
@@ -86,19 +64,16 @@ var _ = Describe("Client", func() {
 		})
 
 		It("delete and then list collections", func() {
-			client, err := chroma.NewClient("http://localhost:8000")
-			Expect(err).ToNot(HaveOccurred())
-
 			// delete when collection doesn't exist should error
-			err = client.DeleteCollection("unittest")
+			err := testClient.DeleteCollection("unittest")
 			Expect(err.Error()).To(Equal("error deleting collection: ValueError('Collection unittest does not exist.')"))
 
 			// create a collection, then delete
-			_, err = client.CreateCollection("unittest", "l2", nil)
+			_, err = testClient.CreateCollection("unittest", "l2", nil)
 			Expect(err).ToNot(HaveOccurred())
 
 			// this time delete shouldn't error
-			err = client.DeleteCollection("unittest")
+			err = testClient.DeleteCollection("unittest")
 			Expect(err).ToNot(HaveOccurred())
 		})
 	})
--- gochromadb_suite_test.go
+++ gochromadb_suite_test.go
@@ -1,13 +1,21 @@
 package chroma_test
 
 import (
+	chroma "github.com/urjitbhatia/gochroma"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
+var testClient *chroma.Client
+
 func TestGochromadb(t *testing.T) {
 	RegisterFailHandler(Fail)
+	BeforeSuite(func() {
+		c, err := chroma.NewClient("http://localhost:8000")
+		Expect(err).ToNot(HaveOccurred())
+		testClient = c
+	})
 	RunSpecs(t, "Gochromadb Suite")
 }
