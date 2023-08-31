bf47900c37cd0de4e02c2e4e9fede9bc04d94cad
Urjit Singh Bhatia <urjitsb87@gmail.com>
2023-08-30 17:15:39 -0700

    refactor testing, use ordered test nodes

--- client_test.go
+++ client_test.go
@@ -34,47 +34,54 @@ var _ = Describe("Client", func() {
 	})
 
 	Describe("collections", func() {
-		BeforeEach(func() {
-			testClient.Reset()
+
+		Describe("create, list, get collection", Ordered, func() {
+			It("reset", func() {
+				ok, err := testClient.Reset()
+				Expect(ok).To(BeTrue())
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+			It("create", func() {
+				// create new collection
+				collection, err := testClient.CreateCollection("unit-test", "l2", nil)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(collection.Name).To(Equal("unit-test"))
+			})
+
+			It("create existing", func() {
+				// should error if recreating existing collection
+				_, err := testClient.CreateCollection("unit-test", "l2", nil)
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal("error while creating collection: ValueError('Collection unit-test already exists.')"))
+			})
+
+			It("list", func() {
+				// list the collections
+				collections, err := testClient.ListCollections()
+				Expect(err).ToNot(HaveOccurred())
+				Expect(len(collections)).To(Equal(1))
+				Expect(collections[0].Name).To(Equal("unit-test"))
+				Expect(collections[0].Metadata).To(Equal(map[string]any{"hnsw:space": "l2"}))
+			})
+
+			It("delete", func() {
+				// delete when collection doesn't exist should error
+				err := testClient.DeleteCollection("unknown")
+				Expect(err.Error()).To(Equal("error deleting collection: ValueError('Collection unknown does not exist.')"))
+
+				// existing collection delete
+				err = testClient.DeleteCollection("unit-test")
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+			It("list after delete", func() {
+				// list the collections
+				collections, err := testClient.ListCollections()
+				Expect(err).ToNot(HaveOccurred())
+				Expect(len(collections)).To(Equal(0))
+			})
 		})
 
-		It("list collections", func() {
-			collections, err := testClient.ListCollections()
-			Expect(err).ToNot(HaveOccurred())
-			Expect(len(collections)).To(Equal(0))
-		})
-
-		It("create and then list collection", func() {
-			// create new collection
-			collection, err := testClient.CreateCollection("unit-test", "l2", nil)
-			Expect(err).ToNot(HaveOccurred())
-			Expect(collection.Name).To(Equal("unit-test"))
-
-			// should error if recreating existing collection
-			_, err = testClient.CreateCollection("unit-test", "l2", nil)
-			Expect(err).To(HaveOccurred())
-			Expect(err.Error()).To(Equal("error while creating collection: ValueError('Collection unit-test already exists.')"))
-
-			// list the collections
-			collections, err := testClient.ListCollections()
-			Expect(err).ToNot(HaveOccurred())
-			Expect(len(collections)).To(Equal(1))
-			Expect(collections[0].Name).To(Equal("unit-test"))
-			Expect(collections[0].Metadata).To(Equal(map[string]any{"hnsw:space": "l2"}))
-		})
-
-		It("delete and then list collections", func() {
-			// delete when collection doesn't exist should error
-			err := testClient.DeleteCollection("unittest")
-			Expect(err.Error()).To(Equal("error deleting collection: ValueError('Collection unittest does not exist.')"))
-
-			// create a collection, then delete
-			_, err = testClient.CreateCollection("unittest", "l2", nil)
-			Expect(err).ToNot(HaveOccurred())
-
-			// this time delete shouldn't error
-			err = testClient.DeleteCollection("unittest")
-			Expect(err).ToNot(HaveOccurred())
-		})
 	})
 })
